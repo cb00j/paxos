920aef2a2ef688d45ae6866c17601c5e12e17d0f
cbj <564773776@qq.com>
2016-07-25 23:19:56 +0800

    paxos

---
+++ src/main/java/com/zavakid/paxos/impl/DefaultAcceptor.java
@@ -0,0 +1,104 @@
+/*
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+package com.zavakid.paxos.impl;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
+import com.zavakid.paxos.Accepted;
+import com.zavakid.paxos.Acceptor;
+import com.zavakid.paxos.Promise;
+import com.zavakid.paxos.util.MDCs;
+
+/**
+ * @author zavakid 2013-4-20 下午8:29:14
+ * @since 1.0
+ */
+public class DefaultAcceptor implements Acceptor {
+
+	private static final Logger LOG = LoggerFactory.getLogger(DefaultAcceptor.class);
+
+	private static final AtomicInteger SEQUENCE = new AtomicInteger();
+	private static final String NAME_PREFIX = "acceptor_";
+
+	private final String name;
+	private Map<Object, Object> values = new ConcurrentHashMap<>();
+	private Map<Object, Long> lastestEpoches = new ConcurrentHashMap<>();
+
+	public DefaultAcceptor() {
+		this(NAME_PREFIX + SEQUENCE.getAndIncrement());
+	}
+
+	public DefaultAcceptor(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public synchronized Promise prepare(Long epoch, Object var) {
+		MDC.put(MDCs.MDC_NAME, this.name);
+		LOG.info("receive prepare, epoch [{}], var [{}]", epoch, var);
+		Long preEpoch = lastestEpoches.get(var);
+		
+		//此Acceptor没有接受任何Proposer的prepare
+		if (preEpoch == null) {
+			LOG.info("ACK prepare for var [{}],epoch [{}],  no preEpoch", var, epoch);
+			lastestEpoches.put(var, epoch);
+			return Promise.create(null, var, null);
+		}
+
+		Object oldValue = values.get(var);
+		//Acceptor 接受了比当前请求的epoch更大的prepare
+		if (preEpoch > epoch) {
+			LOG.info("NAK prepare fro var [{}], preEpoch [{}] is greater than epoch [{}]", var, preEpoch, epoch);
+			//拒绝此次Proposer,返回此前接受的prepare
+			return Promise.create(preEpoch, var, oldValue, true);
+		}
+
+		//承认提交的prepare var
+		LOG.info("ACK accept for var [{}], epoch [{}], preEpoch [{}]", var, epoch, preEpoch);
+		lastestEpoches.put(var, epoch);
+		return Promise.create(preEpoch, var, oldValue);
+	}
+
+	@Override
+	public synchronized Accepted accept(Long epoch, Object var, Object value) {
+		MDC.put(MDCs.MDC_NAME, this.name);
+		LOG.info("receive accept, epoch [{}], var [{}], value [{}]", epoch, var, value);
+		Long preEpoch = lastestEpoches.get(var);
+		if (preEpoch == null) {
+			String msg = "I(the Acceptor) need an epoch before I can accept";
+			LOG.error(msg);
+			throw new IllegalStateException(msg);
+		}
+
+		Object oldValue = values.get(var);
+		if (preEpoch > epoch) {
+			LOG.info(
+					"NAK accept for var [{}], preEpoch is greater! current : (epoch [{}],value [{}]), pre (repEpoch [{}], preValue [{}] )",
+					var, epoch, value, preEpoch, oldValue);
+			return Accepted.create(preEpoch, var, oldValue, true);
+		}
+
+		LOG.info("ACK accept for var [{}], current : (epoch [{}],value [{}]), pre (repEpoch [{}], preValue [{}] )", var,
+				epoch, value, preEpoch, oldValue);
+		values.put(var, value);
+		return Accepted.create(epoch, var, value);
+
+	}
+}
---
+++ src/main/java/com/zavakid/paxos/impl/DefaultProposer.java
@@ -0,0 +1,286 @@
+/*
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+package com.zavakid.paxos.impl;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.ThreadPoolExecutor.CallerRunsPolicy;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.locks.LockSupport;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
+import com.zavakid.paxos.Accepted;
+import com.zavakid.paxos.Acceptor;
+import com.zavakid.paxos.Promise;
+import com.zavakid.paxos.Proposer;
+import com.zavakid.paxos.util.Asserts;
+import com.zavakid.paxos.util.Counter;
+import com.zavakid.paxos.util.MDCs;
+
+/**
+ * @author zavakid 2013-4-20 下午9:18:11
+ * @since 1.0
+ */
+public class DefaultProposer implements Proposer {
+
+	private static final Logger LOG = LoggerFactory.getLogger(DefaultProposer.class);
+	private static final String NAME_PREFIX = "proposer_";
+
+	// 当发生冲突时，沉睡后再发起下一次请求
+	private long sleepTimeWhenNeedNextRound = TimeUnit.NANOSECONDS.convert(10, TimeUnit.MILLISECONDS);
+	private AtomicLong lastEpoch = new AtomicLong(-1L);
+	private String name;
+	private Set<Acceptor> acceptors = new HashSet<>();
+	private int majorityAcceptorNum;
+	private Long proposerId;
+	private Integer proposerNum;
+	private ThreadPoolExecutor executor;
+
+	public DefaultProposer(Set<Acceptor> acceptors, long proposerId, int proposerNum, int threads) {
+		this.name = NAME_PREFIX + proposerId;
+		this.acceptors = acceptors;
+		this.majorityAcceptorNum = acceptors.size() / 2 + 1;
+		this.proposerId = proposerId;
+		this.proposerNum = proposerNum;
+		this.executor = new ThreadPoolExecutor(threads, threads, 0L, TimeUnit.SECONDS,
+				new ArrayBlockingQueue<Runnable>(threads), new CallerRunsPolicy());
+	}
+	
+	@Override
+	public Object propose(final Object var, final Object value) {
+		final Long epoch = generateEpoch(0L, var);
+		return proposeWithEpoch(epoch, var, value);
+	}
+
+	public Object proposeWithEpoch(final Long epoch, final Object var, final Object value) {
+		MDC.put(MDCs.MDC_NAME, this.name);
+		LOG.info("start to send prepare (epoch[{}], var [{}], value [{}])", epoch, var, value);
+		//phase 1
+		List<Promise> promises = concurrentPrepare(var, epoch);
+		// 有可能是网络出现中断引起的
+		if (promises.size() < this.majorityAcceptorNum) {
+			LOG.info("receive promises num [{}] , less then majorityAcceptorNum [{}], try to propose again",
+					promises.size(), this.majorityAcceptorNum);
+			proposeWithEpoch(epoch, var, value);
+		}
+		
+		//Acceptor 拒绝接收的Propose个数
+		int nakPromisesNum = 0;
+		Long maxEpoch = epoch;
+		//此Propose是Acceptor第一个接收的个数
+		int firstPromises = 0;
+		//Acceptor接收了Propose,并且此前Acceptor还接受了其他的Propose，只是此Propose的epoch大于其他提交的
+		int maybeHasValuePromises = 0;
+		
+		Counter<Object> values = new Counter<>();
+		for (Promise promise : promises) {
+			// acceptor 的 epoch 比此 epoch 要大
+			if (promise.isNAK()) {
+				nakPromisesNum++;
+				maxEpoch = maxEpoch > promise.getPreEpoch() ? maxEpoch : promise.getPreEpoch();
+				continue;
+			}
+			// acceptor 没有 epoch
+			if (promise.getPreEpoch() == null) {
+				firstPromises++;
+				continue;
+			}
+			// acceptor 的 epoch 比此 epoch 要小或者相等
+			if (promise.getPreEpoch() <= epoch) {
+				maybeHasValuePromises++;
+				values.add(promise.getValue());
+				continue;
+			}
+
+			Asserts.unreachable();
+		}
+
+		LOG.info(
+				"prepare (epoch [{}], var [{}], value [{}]) status  : nakPromisesNum [{}], maxEpoch [{}], firstPromises [{}], maybeHasValuePromises [{}], this.majorityAcceptorNum [{}]",
+				epoch, var, value, nakPromisesNum, maxEpoch, firstPromises, maybeHasValuePromises,
+				this.majorityAcceptorNum);
+		if (nakPromisesNum >= this.majorityAcceptorNum) {
+			LOG.info(
+					"prepare fail for var [{}], value [{}], nakPromisesNum [{}] is greater then majorityAcceptorNum [{}], the max preEpoch is [{}]",
+					var, value, nakPromisesNum, majorityAcceptorNum, maxEpoch);
+			return nextRound(maxEpoch, var, value);
+		}
+
+		// prepare success, and we can continue phase 2
+		if (firstPromises >= this.majorityAcceptorNum) {
+			LOG.info(
+					"prepare success for var [{}], value [{}], firstPromises [{}] is greater then majorityAcceptorNum [{}], try accept with epoch [{}]",
+					var, value, firstPromises, this.majorityAcceptorNum, epoch);
+			return tryAccept(epoch, var, value);
+		}
+
+		if (maybeHasValuePromises >= this.majorityAcceptorNum
+				|| (firstPromises + maybeHasValuePromises) >= this.majorityAcceptorNum) {
+			Object newValue = values.getMostItem();
+			if (newValue == null) {
+				newValue = value;
+			}
+
+			LOG.info("prepare success for var [{}], epoch [{}], we can try accept with new value [{}]", var, epoch,
+					newValue);
+			return tryAccept(epoch, var, newValue);
+		}
+
+		return nextRound(maxEpoch, var, value);
+
+	}
+
+	public Object tryAccept(final Long epoch, final Object var, final Object value) {
+		List<Accepted> accepteds = concurrentCommit(epoch, var, value);
+		// 网络中断，重新发起一次
+		if (accepteds.size() < this.majorityAcceptorNum) {
+			return proposeWithEpoch(epoch, var, value);
+		}
+
+		int nakAcceptedNum = 0;
+		int successAcceptedNum = 0;
+		Long maxEpochWhenAccepted = epoch;
+		Counter<Object> oldAcceptedValues = new Counter<>();
+		for (Accepted accepted : accepteds) {
+			// 说明别的 Proposer 预约了更新的 epoch
+			if (accepted.isNAK()) {
+				nakAcceptedNum++;
+				maxEpochWhenAccepted = maxEpochWhenAccepted > accepted.getEpoch() ? maxEpochWhenAccepted
+						: accepted.getEpoch();
+				oldAcceptedValues.add(accepted.getValue());
+				continue;
+			}
+
+			// 成功占领
+			if (!accepted.isNAK()) {
+				successAcceptedNum++;
+				oldAcceptedValues.add(accepted.getValue());
+				continue;
+			}
+			Asserts.unreachable();
+		}
+
+		LOG.info(
+				"accept (epoch [{}], var [{}], value [{}]) status : nakAcceptedNum [{}], successAcceptedNum [{}], maxEpochWhenAccepted [{}], majorityAcceptorNum [{}]",
+				epoch, var, value, nakAcceptedNum, successAcceptedNum, maxEpochWhenAccepted, this.majorityAcceptorNum);
+
+		if (nakAcceptedNum >= this.majorityAcceptorNum) {
+			LOG.info("NAK accept, epoch [{}], var [{}], value[{}], the maxEpochWhenAccepted is [{}]", epoch, var, value,
+					maxEpochWhenAccepted);
+			return nextRound(maxEpochWhenAccepted, var, value);
+		}
+
+		if (successAcceptedNum >= this.majorityAcceptorNum) {
+			LOG.info("ACK accept, epoch [{}], var [{}], value[{}]", epoch, var, value);
+			return oldAcceptedValues.getMostItem();
+		}
+
+		// 跑到这里，有可能是有偶数个 acceptor，并且 nakAcceptedNum == successAcceptedNum
+		// 所以认为没有超过一半 acceptor 同意，因此需要进行下一轮
+		return nextRound(maxEpochWhenAccepted, var, value);
+	}
+
+	protected List<Promise> concurrentPrepare(final Object var, final Long epoch) {
+		CompletionService<Promise> completionService = new ExecutorCompletionService<>(this.executor);
+		for (final Acceptor acceptor : acceptors) {
+			completionService.submit(() -> acceptor.prepare(epoch, var));
+		}
+
+		List<Promise> promises = new ArrayList<>(acceptors.size());
+		for (int i = 0; i < acceptors.size(); i++) {
+			Future<Promise> future = null;
+			try {
+				future = completionService.take();
+				Promise promise = future.get();
+				promises.add(promise);
+			} catch (InterruptedException e) {
+				throw new RuntimeException("propose (phase 1) was interrupted", e);
+			} catch (ExecutionException e) {
+				processExecutionExeception(e);
+			}
+		}
+		return promises;
+	}
+
+	protected List<Accepted> concurrentCommit(final Long epoch, final Object var, final Object value) {
+		CompletionService<Accepted> completionService = new ExecutorCompletionService<>(this.executor);
+		for (final Acceptor acceptor : acceptors) {
+			completionService.submit(() -> acceptor.accept(epoch, var, value));
+		}
+
+		List<Accepted> accepteds = new ArrayList<>(acceptors.size());
+		for (int i = 0; i < acceptors.size(); i++) {
+			Future<Accepted> future = null;
+			try {
+				future = completionService.take();
+				Accepted accepted = future.get();
+				accepteds.add(accepted);
+			} catch (InterruptedException e) {
+				throw new RuntimeException("propose (phase 2) was interrupted", e);
+			} catch (ExecutionException e) {
+				processExecutionExeception(e);
+			}
+		}
+		return accepteds;
+	}
+
+	protected Object nextRound(Long maxEpoch, Object var, Object value) {
+		LockSupport.parkNanos(this.sleepTimeWhenNeedNextRound);
+		Long newEpoch = generateEpoch(maxEpoch, var);
+		return proposeWithEpoch(newEpoch, var, value);
+	}
+
+	protected void processExecutionExeception(ExecutionException e) {
+		e.printStackTrace();
+	}
+
+	/**
+	 * preEpoch 可能是从 EPOCH_SEQ.getAndIncrement(); 获得<br/>
+	 * 也可能是从上次 prepare/acceptor 中获得的最大 epoch <br/>
+	 */
+	synchronized protected Long generateEpoch(Long preEpoch, Object var) {
+		long lastEpoch = this.lastEpoch.get();
+		if (preEpoch > lastEpoch) {
+			long epoch = (preEpoch / this.proposerNum + 1) * this.proposerNum + this.proposerId;
+			this.lastEpoch.set(epoch);
+			return epoch;
+		} else {
+			long epoch = ((lastEpoch / this.proposerNum + 1) * this.proposerNum) + this.proposerId;
+			this.lastEpoch.set(epoch);
+			return epoch;
+		}
+	}
+
+	@Override
+	public void stop() {
+		this.executor.shutdown();
+		try {
+			this.executor.awaitTermination(Long.MAX_VALUE, TimeUnit.MILLISECONDS);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
